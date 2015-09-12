056396e59b1643f7323989f88c0b79346ae65af6
DeborahChapdelaine <debc2007@gmail.com>
2015-09-12 11:48:08 -0500

    fixing README.md description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chatter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "storychat is a place to tell your life stories"
+  :url "https://github.com/deborahchapdelaine/Chatter91215"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
